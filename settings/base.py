a36f1a7814c5dd22b914b953b9c73badf3c2726e
A.Provotorova <Fyfcnfcbz14081993>
2021-07-22 13:23:34 +0400

    Fix big mistake

--- settings/base.py
+++ settings/base.py
@@ -7,6 +7,3 @@ env = Env()
 ROLE = env.str('ROLE', default='local')
 BASE_PATH = Path.cwd().absolute()
 DEBUG = env.bool('DEBUG', default=True)
-
-if ROLE == 'local':
-    env.read_env(path=str(BASE_PATH / '.env'))
