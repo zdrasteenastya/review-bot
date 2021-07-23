d8f5b6c69a36ab223cde6123ad58c1a5343a8281
A.Provotorova <Fyfcnfcbz14081993>
2021-07-23 12:46:15 +0400

    Fix config

--- settings/bot.py
+++ settings/bot.py
@@ -3,7 +3,7 @@ from settings.base import env
 
 class TgBotConfig(object):
     TOKEN = env.str('TG_TOKEN', default='')
-    CHAT_ID_FOR_WORKERS = '-440211705'
+    CHAT_ID_FOR_WORKERS = env.str('CHAT_ID', default='')
     WELCOME_MESSAGE = 'День добрый :)'
     EVERY_DAY_TEXT = 'По МР сегодня: @{user_1} и @{user_2}'
     ADD_ME = 'Запомнил тебя, {f_name} {s_name}'
